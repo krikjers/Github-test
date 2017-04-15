46d4664d041f4ac90faa9486cb913ed5a16aa23a
krikjers <krikjers@stud.ntnu.no>
2017-04-15 11:23:54 +0200

    Added print function to util
    tested and works

---
+++ githubtest/util.cpp
@@ -0,0 +1,12 @@
+
+#include <iostream>
+#include "util.h"
+#include <string>
+
+using namespace std;
+
+
+void print(string msg)
+{
+	cout << msg;
+}
--- githubtest/util.h
+++ githubtest/util.h
@@ -1,1 +1,5 @@
 #pragma once
+
+#include <string>
+using namespace std;
+void print(string msg);
